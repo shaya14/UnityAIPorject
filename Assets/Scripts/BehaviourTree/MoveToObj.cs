9a0e8d752004dfcf5cfe86afb17add2ed33e638c
shaya14 <84465665+shaya14@users.noreply.github.com>
2023-06-08 01:24:45 +0300

    Move To Obj

--- Assets/Scripts/BehaviourTree/MoveToObj.cs
+++ Assets/Scripts/BehaviourTree/MoveToObj.cs
@@ -1,18 +1,53 @@
+using BehaviorDesigner.Runtime;
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MoveToObj : MonoBehaviour
+public class MoveToObj : Action
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent _agent;
+
+    public SharedTransform _targetObj;
+    public override void OnAwake()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+    public override void OnStart()
+    {
+        if (isInRange(transform.position, _targetObj.Value.position, 0.1f) == false)
+        {
+            _agent.SetDestination(_targetObj.Value.position);
+            _agent.isStopped = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override TaskStatus OnUpdate()
     {
-        
+        if (_targetObj.Value == null)
+        {
+            return TaskStatus.Failure;
+        }
+
+        if (_agent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+            _agent.SetDestination(_targetObj.Value.position);
+            _agent.isStopped = false;
+        }
+
+        if (isInRange(transform.position, _targetObj.Value.position, 1))
+        {
+            _agent.isStopped = true;
+            return TaskStatus.Success;
+        }
+        return TaskStatus.Running;
+    }
+
+    public static bool isInRange(Vector3 origin, Vector3 target, float range)
+    {
+        return Vector3.Distance(origin, target) <= range;
     }
 }
